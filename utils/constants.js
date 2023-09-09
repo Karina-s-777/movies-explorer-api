596f3af0980958845f03feecf6400f1be99a9e98
Karina Savilova <savilovakarina@yandex.ru>
2023-09-09 19:40:46 +0200

    Изменить константы ошибок

--- utils/constants.js
+++ utils/constants.js
@@ -4,11 +4,11 @@ const INCORRECT_URL = 'Некорректный URL';
 
 const INTERNAL_SERVER_ERROR = 'Внутренняя ошибка сервера';
 
-const UNAUTHORIZED = 'Необходима вторизация';
-const BAD_REQUEST = 'Bad request';
-const FORBIDDEN = 'Forbidden';
+const UNAUTHORIZED = 'Необходима аторизация';
+const BAD_REQUEST = 'Сервер не смог обработать ваш запрос';
+const FORBIDDEN = 'Недостаточно прав доступа';
 const NOT_FOUND = 'Ресурс не найден';
-const CONFLICT = 'Conflict';
+const CONFLICT = 'Не удается обработать запрос из-за конфликта в текущем состоянии ресурса';
 
 const EMAIL_EXISTS = 'Пользователь с такой почтой уже существует';
 
